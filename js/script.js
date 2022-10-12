bccba14ab73ec12f67da1f57ce1916aaa5dd5471
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-12 17:11:12 +0200

    bonus 2

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ da rendere visibile l'elemento-immagine che ha quella classe*/
                nextArrw.classList.add("hidden");
             } 
             */
-        } //Altrimenti l'array riparte 
+        } //Altrimenti l'array riparte dalla prima immagine
         else { 
             items[position].classList.remove("active");
             position = 0;
@@ -81,7 +81,9 @@ da rendere visibile l'elemento-immagine che ha quella classe*/
                 prevArrw.classList.add("hidden");
             }
             */
-        } else {
+
+        } //altrimenti l'array riparte dall'ultima immagine
+        else {
                 items[position].classList.remove("active");
                 position = items.length-1;
                 items[position].classList.add("active");
@@ -93,9 +95,6 @@ da rendere visibile l'elemento-immagine che ha quella classe*/
 
 
 
-// **BONUS 1:**
-// Aggiungere il **ciclo infinito** del carosello. Ovvero se è attiva la prima immagine e l'utente 
-//clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
 
 // **BONUS 2:**
 // Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, 
@@ -103,12 +102,7 @@ da rendere visibile l'elemento-immagine che ha quella classe*/
 //tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
 // Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
 
-// **Prima di partire a scrivere codice:**
-// Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, 
-//cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS,
-// se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. 
 
-//Non dedichiamo però al ripasso più di una **mezz'ora**, così da non perdere di vista il focus dell'esercizio.
 // **Consigli del giorno:**
 // 1. Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
 // 2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
